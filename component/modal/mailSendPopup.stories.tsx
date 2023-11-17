bd60f215a195bdf466c543dc9c1591a746c3bbec
Tae-woong Kim <127489800+futureplanning@users.noreply.github.com>
2023-11-17 13:38:40 +0900

    Update mailSendPopup.stories.tsx

--- component/modal/mailSendPopup.stories.tsx
+++ component/modal/mailSendPopup.stories.tsx
@@ -4,7 +4,6 @@ import { css } from '@emotion/react';
 import StoryLayout from '@ComponentFarm/modules/story_layout/StoryLayout';
 import { Button } from '@ComponentFarm/atom/Button/Button';
 import MailSendPopup from './mailSendPopup';
-import SearchPopup from './SearchPopup/SearchPopup';
 
 const meta: Meta = {
   title: 'Modal/mailSendPopup',
@@ -71,125 +70,125 @@ const StoryStorePopup: Story<Props> = args => {
 };
 export const Default = StoryStorePopup.bind({});
 
-const ResultTempArr2 = [
-  {
-    id: 0,
-    kind: '시가지',
-    name: '광화문',
-    date: '2023-10-20',
-    state: 'op',
-  },
-  {
-    id: 1,
-    kind: '시가지',
-    name: '강남점',
-    date: '2023-10-20',
-    state: 'op',
-  },
-  {
-    id: 2,
-    kind: '직영점',
-    name: '홍대점',
-    date: '2023-10-20',
-    state: 'st',
-  },
-  {
-    id: 3,
-    kind: '직영점',
-    name: '개봉점',
-    date: '2023-10-20',
-    state: 'st',
-  },
-  {
-    id: 4,
-    kind: 'X-GOPIZZA',
-    name: '철산점',
-    date: '2023-10-20',
-    state: 'st',
-  },
-];
+// const ResultTempArr2 = [
+//   {
+//     id: 0,
+//     kind: '시가지',
+//     name: '광화문',
+//     date: '2023-10-20',
+//     state: 'op',
+//   },
+//   {
+//     id: 1,
+//     kind: '시가지',
+//     name: '강남점',
+//     date: '2023-10-20',
+//     state: 'op',
+//   },
+//   {
+//     id: 2,
+//     kind: '직영점',
+//     name: '홍대점',
+//     date: '2023-10-20',
+//     state: 'st',
+//   },
+//   {
+//     id: 3,
+//     kind: '직영점',
+//     name: '개봉점',
+//     date: '2023-10-20',
+//     state: 'st',
+//   },
+//   {
+//     id: 4,
+//     kind: 'X-GOPIZZA',
+//     name: '철산점',
+//     date: '2023-10-20',
+//     state: 'st',
+//   },
+// ];
 
-const columnName = {
-  searchBoxTitle: ['매장 상태', '매장 분류'],
-  th: ['매장 분류', '매장명', '매장 오픈일', '매장 상태'],
-  col: [46, 130, 100, 190, 180],
-};
+// const columnName = {
+//   searchBoxTitle: ['매장 상태', '매장 분류'],
+//   th: ['매장 분류', '매장명', '매장 오픈일', '매장 상태'],
+//   col: [46, 130, 100, 190, 180],
+// };
 
-const StoryStorePopup2: Story<Props> = args => {
-  const [isOpen, setIsOpen] = useState(false);
+// const StoryStorePopup2: Story<Props> = args => {
+//   const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
-    if (isOpen) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'unset';
-    }
-  }, [isOpen]);
+//   useEffect(() => {
+//     if (isOpen) {
+//       document.body.style.overflow = 'hidden';
+//     } else {
+//       document.body.style.overflow = 'unset';
+//     }
+//   }, [isOpen]);
 
-  const handlerClose = () => {
-    setIsOpen(false);
-  };
+//   const handlerClose = () => {
+//     setIsOpen(false);
+//   };
 
-  return (
-    <StoryLayout
-      {...args}
-      customCss={css`
-        height: 500px;
-      `}
-    >
-      <div>
-        <Button variant="primary" onClick={() => setIsOpen(true)}>
-          다른 곳에서 사용될때
-        </Button>
-        <SearchPopup
-          title="매장 검색"
-          columnName={columnName}
-          data={ResultTempArr2}
-          isOpen={isOpen}
-          onClose={handlerClose}
-        />
-      </div>
-    </StoryLayout>
-  );
-};
-export const Various = StoryStorePopup2.bind({});
+//   return (
+//     <StoryLayout
+//       {...args}
+//       customCss={css`
+//         height: 500px;
+//       `}
+//     >
+//       <div>
+//         <Button variant="primary" onClick={() => setIsOpen(true)}>
+//           다른 곳에서 사용될때
+//         </Button>
+//         <SearchPopup
+//           title="매장 검색"
+//           columnName={columnName}
+//           data={ResultTempArr2}
+//           isOpen={isOpen}
+//           onClose={handlerClose}
+//         />
+//       </div>
+//     </StoryLayout>
+//   );
+// };
+// export const Various = StoryStorePopup2.bind({});
 
-const StoryStorePopup3: Story<Props> = args => {
-  const [isOpen, setIsOpen] = useState(false);
+// const StoryStorePopup3: Story<Props> = args => {
+//   const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
-    if (isOpen) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'unset';
-    }
-  }, [isOpen]);
+//   useEffect(() => {
+//     if (isOpen) {
+//       document.body.style.overflow = 'hidden';
+//     } else {
+//       document.body.style.overflow = 'unset';
+//     }
+//   }, [isOpen]);
 
-  const handlerClose = () => {
-    setIsOpen(false);
-  };
+//   const handlerClose = () => {
+//     setIsOpen(false);
+//   };
 
-  return (
-    <StoryLayout
-      {...args}
-      customCss={css`
-        height: 500px;
-      `}
-    >
-      <div>
-        <Button variant="primary" onClick={() => setIsOpen(true)}>
-          라디오 스타일
-        </Button>
-        <SearchPopup
-          title="매장 검색"
-          type="radio"
-          columnName={columnName}
-          data={ResultTempArr2}
-          isOpen={isOpen}
-          onClose={handlerClose}
-        />
-      </div>
-    </StoryLayout>
-  );
-};
-export const RadioSty = StoryStorePopup3.bind({});
+//   return (
+//     <StoryLayout
+//       {...args}
+//       customCss={css`
+//         height: 500px;
+//       `}
+//     >
+//       <div>
+//         <Button variant="primary" onClick={() => setIsOpen(true)}>
+//           라디오 스타일
+//         </Button>
+//         <SearchPopup
+//           title="매장 검색"
+//           type="radio"
+//           columnName={columnName}
+//           data={ResultTempArr2}
+//           isOpen={isOpen}
+//           onClose={handlerClose}
+//         />
+//       </div>
+//     </StoryLayout>
+//   );
+// };
+// export const RadioSty = StoryStorePopup3.bind({});
