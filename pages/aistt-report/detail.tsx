470b0141bb6079d703f3c01eb0f46a5405b14a84
futureplanning <futureplanning@gopizza.kr>
2023-12-14 17:31:09 +0900

    MailSendPopup 에러 테스트

--- pages/aistt-report/detail.tsx
+++ pages/aistt-report/detail.tsx
@@ -12,7 +12,7 @@ import {
 import { Button } from '@ComponentFarm/atom/Button/Button';
 import { Tabs } from '@ComponentFarm/atom/Tab/Tab';
 import TitleArea from '@ComponentFarm/layout/TitleArea';
-import MailSendPopup, { IMailData } from '@ComponentFarm/modal/MailSendPopup';
+// import MailSendPopup, { IMailData } from '@ComponentFarm/modal/MailSendPopup';
 import { ManufacturingQualityList } from '@ComponentFarm/template/aistt/common/ManufacturingQuality';
 import { aisttStateListTabData } from '@ComponentFarm/template/aistt/const';
 import AnalysisResult from '@ComponentFarm/template/aistt/report/detail/AnalysisResult';
@@ -189,12 +189,12 @@ export const Detail = () => {
           />
         </AreaBox>
       </DetailInfoWrap>
-      <MailSendPopup
+      {/* <MailSendPopup
         initial_recv_emails={[{ label: '', value: '' }]}
         isOpen={mailOpen}
         onClose={mailOpenHandler}
         submitFunc={mailSendSubmit}
-      />
+      /> */}
       <MailSendListPopup
         fqs_reports_idx={String(id)}
         isOpen={mailListOpen}
