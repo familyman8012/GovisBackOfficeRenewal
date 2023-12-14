a7ee986268942b41faab67385dc6e7b3200cb1ce
futureplanning <futureplanning@gopizza.kr>
2023-12-14 17:40:39 +0900

    MailSendPopup 에러테스트 2-3

--- pages/aistt-report/detail.tsx
+++ pages/aistt-report/detail.tsx
@@ -12,7 +12,7 @@ import {
 import { Button } from '@ComponentFarm/atom/Button/Button';
 import { Tabs } from '@ComponentFarm/atom/Tab/Tab';
 import TitleArea from '@ComponentFarm/layout/TitleArea';
-import MailSendPopup from '@ComponentFarm/modal/MailSendPopup';
+import MailSendPopup, { IMailData } from '@ComponentFarm/modal/mailSendPopup';
 import { ManufacturingQualityList } from '@ComponentFarm/template/aistt/common/ManufacturingQuality';
 import { aisttStateListTabData } from '@ComponentFarm/template/aistt/const';
 import AnalysisResult from '@ComponentFarm/template/aistt/report/detail/AnalysisResult';
@@ -88,7 +88,7 @@ export const Detail = () => {
     setMailOpen(prev => !prev);
   };
 
-  const mailSendSubmit = async (data: any) => {
+  const mailSendSubmit = async (data: IMailData) => {
     const { recipient, ...mailSendData } = data;
     const recv_emails = recipient?.map(item => item.value).join(',');
     ReportSendMailSubmit.mutate({
