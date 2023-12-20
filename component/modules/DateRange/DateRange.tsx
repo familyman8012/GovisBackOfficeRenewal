b1506c9b3275a45b8e78707f0d9dc482e1d1a8a4
futureplanning <futureplanning@gopizza.kr>
2023-12-20 18:47:02 +0900

    데이터 레인지 어제 버튼 추가

--- component/modules/DateRange/DateRange.tsx
+++ component/modules/DateRange/DateRange.tsx
@@ -240,6 +240,9 @@ const DateRangePicker: React.FC<DateRangePickerProps> = ({
               />
             </div>
             <div className="box_btn">
+              <button type="button" onClick={() => setRangeSetting(0)}>
+                어제
+              </button>
               <button type="button" onClick={() => setRangeSetting(1)}>
                 오늘
               </button>
