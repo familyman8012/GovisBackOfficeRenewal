173523ee2f2b9d592be3d99638da2dafa6a098f1
futureplanning <futureplanning@gopizza.kr>
2023-12-21 13:27:20 +0900

    제조현황 - 썸네일

--- component/template/common/NumberBarList.tsx
+++ component/template/common/NumberBarList.tsx
@@ -28,11 +28,12 @@ export const NumberBarWrap = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    overflow: hidden;
     width: 9.7rem;
     width: 5rem;
     height: 5rem;
     margin: 0 2.4rem;
-    padding: 1.3rem;
+    padding: 0;
     border-radius: 0.8rem;
     background: var(--color-neutral90);
 
