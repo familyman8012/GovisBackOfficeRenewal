9ef82eddcaf8130f41ff6df08fd873dcda3670da
futureplanning <futureplanning@gopizza.kr>
2024-02-28 17:44:45 +0900

    columnWidth 계산시, 새롭게 mount

--- component/modules/VirtualizedTable/VirtualizedTable.tsx
+++ component/modules/VirtualizedTable/VirtualizedTable.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { AutoSizer, GridCellProps, MultiGrid } from 'react-virtualized';
 
 interface VirtualizedTableProps {
@@ -22,7 +22,13 @@ const VirtualizedTable = ({
   cellRenderer,
 }: VirtualizedTableProps) => {
   const [gridSize, setGridSize] = useState({ width: 0, height: 0 });
+  const [key, setKey] = useState(0);
   const gridRef = useRef<MultiGrid>(null);
+
+  useEffect(() => {
+    // viewColumn 값이 변경될 때마다 키 상태를 업데이트하여 컴포넌트를 다시 마운트합니다.
+    setKey(prevKey => prevKey + 1);
+  }, [viewColumn]); // viewColumn을 의존성 배열에 추가합니다.
 
   const onResize = ({ width, height }: { width: number; height: number }) => {
     setGridSize({ width, height });
@@ -38,7 +44,7 @@ const VirtualizedTable = ({
         return gridSize.width * 0.12;
       }
       // 나머지 열은 전체 너비의 80%를 나머지 열 수로 나눔
-      const remainingWidth = gridSize.width * 0.78;
+      const remainingWidth = gridSize.width * 0.78 - 48;
       const remainingColumns = viewColumn - 2;
 
       return remainingWidth / remainingColumns;
@@ -59,6 +65,7 @@ const VirtualizedTable = ({
       <AutoSizer onResize={onResize}>
         {({ width, height }) => (
           <MultiGrid
+            key={key}
             ref={gridRef} // 참조 설정
             width={width}
             height={height}
